e1448ab54436d55478f1fad0d8b123792806e9fb
mbsdev <mbsdev2018@gmail.com>
2021-12-06 00:59:06 +0000

    Initial commit

---
+++ src/main/java/com/mbs/spbmkenkins/SpbmkenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.mbs.spbmkenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbmkenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbmkenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbs/spbmkenkins/SpbmkenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbs.spbmkenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbmkenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
